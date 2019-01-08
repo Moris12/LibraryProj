73f11cc2f13263b79ef627a7f10b4745b5910cee
Asaf <Asaf@LAPTOP-7SQ3LKB0>
2019-01-08 16:43:23 +0200

    ani dafuk ki lo raiti she ha lemala lo ...

---
+++ OBLteam15/src/gui/CreateNewMemberPOPLibrarianController.java
@@ -0,0 +1,88 @@
+package gui;
+
+import java.util.LinkedHashMap;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import models.Message;
+
+public class CreateNewMemberPOPLibrarianController {
+
+	Stage Stage;
+	FXMLLoader loader;
+	Pane root;
+	Scene scene;
+	
+    @FXML
+    private TextField CreateNewMemberPUPLibrarian_FirstName_TXF;
+
+    @FXML
+    private TextField CreateNewMemberPUPLibrarian_LastName_TXF;
+
+    @FXML
+    private TextField CreateNewMemberPUPLibrarian_ID_TXF;
+
+    @FXML
+    private TextField CreateNewMemberPUPLibrarian_Email_TXF;
+
+    @FXML
+    private TextField CreateNewMemberPUPLibrarian_PhoneNumber_TXF;
+
+    @FXML
+    private TextField CreateNewMemberPUPLibrarian_DateOfGraduate_TXF;
+
+    @FXML
+    private Button CreateNewMemberPUPLibrarian_Cancel_BTN;
+
+    @FXML
+    private Label CreateNewMemberPUPLibrarian_Done_LB;
+
+    @FXML
+    void CreateNewMemberPUP_Cancel(ActionEvent event) {//CANCEL button handler
+    	Stage stage = (Stage) CreateNewMemberPUPLibrarian_Cancel_BTN.getScene().getWindow();
+	    stage.close();
+    }
+
+    @FXML
+    void CreateNewMemberPUP_DONE(MouseEvent event) { //DONE clicked.
+    	LinkedHashMap<String, Object> map = new LinkedHashMap<String,Object>();
+    	Message msg;
+    	String FirstName, LastName, ID, GraduateDate, Email, PhoneNumber;
+    	if(!CreateNewMemberPUPLibrarian_FirstName_TXF.getText().isEmpty() && !CreateNewMemberPUPLibrarian_LastName_TXF.getText().isEmpty() && !CreateNewMemberPUPLibrarian_ID_TXF.getText().isEmpty() && !CreateNewMemberPUPLibrarian_DateOfGraduate_TXF.getText().isEmpty())
+    	{//check the essential fields.
+    		//set variables with the fields input.
+    		FirstName = CreateNewMemberPUPLibrarian_FirstName_TXF.getText();
+    		LastName = CreateNewMemberPUPLibrarian_LastName_TXF.getText();
+    		ID = CreateNewMemberPUPLibrarian_ID_TXF.getText();
+    		GraduateDate = CreateNewMemberPUPLibrarian_DateOfGraduate_TXF.getText();
+    		
+    		if(!CreateNewMemberPUPLibrarian_Email_TXF.getText().isEmpty())
+    			Email = CreateNewMemberPUPLibrarian_Email_TXF.getText();
+    		else Email = null;
+    		if(CreateNewMemberPUPLibrarian_PhoneNumber_TXF.getText().isEmpty())
+    			PhoneNumber = CreateNewMemberPUPLibrarian_PhoneNumber_TXF.getText();
+    		else PhoneNumber = null;
+    	}
+    }
+
+	public void start(Stage arg0) throws Exception {
+		final Modality WINDOW_MODAL; //makes it appear as pop up that has to be closed.
+		this.Stage = new Stage();
+		//Stage.initModality(WINDOW_MODAL);
+		this.loader = new FXMLLoader();
+		this.root = loader.load(getClass().getResource("/gui/Librarian_MainPage.fxml").openStream());
+		this.scene = new Scene(root);			
+		this.Stage.setScene(scene);
+		this.Stage.showAndWait();
+		
+	}
+}
---
+++ OBLteam15/src/gui/Librarian_MainPageController.java
@@ -0,0 +1,210 @@
+package gui;
+
+import java.util.LinkedHashMap;
+
+import client.Client;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import models.Message;
+
+public class Librarian_MainPageController {
+
+	Stage Stage;
+	FXMLLoader loader;
+	Pane root;
+	Scene scene;
+	Client client;
+	
+    @FXML
+    private Label LibririanMainPage_LibrarianName_LB;
+
+    @FXML
+    private Label LibririanMainPage_LibrarianStatus_LB;
+
+    @FXML
+    private MenuButton LibririanMainPage_InventoryManag_MNBTN;
+
+    @FXML
+    private MenuItem LibririanMainPage_AddNewBook_MNI;
+
+    @FXML
+    private MenuItem LibririanMainPage_RemoveBook_MNI;
+
+    @FXML
+    private MenuItem LibririanMainPage_ViewBookDetails_MNI;
+
+    @FXML
+    private Label LibririanMainPage_Date_LB;
+
+    @FXML
+    private Label LibririanMainPage_Hour_LB;
+
+    @FXML
+    private Button LibririanMainPage_CreatNewMember_BTN;
+
+    @FXML
+    private Rectangle LibririanMainPage_SearchByCategory_TXF;
+
+    @FXML
+    private TextField LibririanMainPage_SearchByBookName_TXF;
+
+    @FXML
+    private TextField LibririanMainPage_SearchByAutorName_TXF;
+
+    @FXML
+    private TextField LibririanMainPage_SearchCategory_TXF;
+
+    @FXML
+    private Button LibririanMainPage_Search_BTN;
+
+    @FXML
+    private TextField LibririanMainPage_SearchByFreeText_TXF;
+
+    @FXML
+    private Label LibririanMainPage_LogOut_LB;
+
+    @FXML
+    private Button LibririanMainPage_ReturnBook_BTN;
+
+    @FXML
+    private Button LibririanMainPage_ViewMemberCard_BTN;
+
+    @FXML
+    private Button LibririanMainPage_Messages_BTN;
+
+    @FXML
+    private RadioButton LibririanMainPage_SearchByBookName_RDBTN;
+
+    @FXML
+    private ToggleGroup Search_group;
+
+    @FXML
+    private RadioButton LibririanMainPage_SearchByAutorName_RDBTN;
+
+    @FXML
+    private RadioButton LibririanMainPage_SearchByCategory_RDBTN;
+
+    @FXML
+    private RadioButton LibririanMainPage_SearchByFreeText_RDBTN;
+
+    @FXML
+    void LaibrarianMainPage_SearchByGroup(ActionEvent event) //search button click function. 
+    //take information from txtfld, and search by the argument given.
+    {
+    	LinkedHashMap<String, Object> map = new LinkedHashMap<String,Object>();
+    	map.put("Action", "Search Book");
+    	Message msg;
+    	String searchVal;
+    	if(Search_group.getSelectedToggle() != null)
+    	{
+    		if(Search_group.getSelectedToggle() == LibririanMainPage_SearchByBookName_RDBTN) //search by name of the book
+    		{
+    			searchVal = LibririanMainPage_SearchByBookName_TXF.getText();
+    			map.put("by", "book name");
+    			map.put("key",searchVal);
+    		}
+    		if(Search_group.getSelectedToggle() == LibririanMainPage_SearchByAutorName_RDBTN) //search by author name
+    		{
+    			searchVal = LibririanMainPage_SearchByAutorName_TXF.getText();
+    			map.put("by", "author name");
+    			map.put("key",searchVal);
+    		}
+    		if(Search_group.getSelectedToggle() == LibririanMainPage_SearchByCategory_RDBTN) //search by categories 
+    		{
+    			searchVal = LibririanMainPage_SearchCategory_TXF.getText();
+    			map.put("by", "category");
+    			map.put("key",searchVal);
+    		}
+    		if(Search_group.getSelectedToggle() == LibririanMainPage_SearchByFreeText_RDBTN) //search by free text
+    		{
+    			searchVal = LibririanMainPage_SearchByFreeText_TXF.getText();
+    			map.put("by", "free text");
+    			map.put("key",searchVal);
+    		}
+    		//end if's.
+    		msg = new Message(map);
+    		//send to server?.
+    	}
+    	else //no radio button selected 
+    	{
+    		System.out.println("no radio button selected.");
+    	}
+    }
+
+    @FXML
+    void LibrarianMainPage_CreateNewMemberCard(ActionEvent event) throws Exception //create new member func 
+    {
+    	CreateNewMemberPOPLibrarianController newMemberForm = new CreateNewMemberPOPLibrarianController();
+    	newMemberForm.start(null); //one day we will send client here
+    }
+
+    @FXML
+    void LibrarianMainPage_Date(MouseEvent event) {
+
+    }
+
+    @FXML
+    void LibrarianMainPage_HelloLibrarianName(MouseEvent event) {
+
+    }
+
+    @FXML
+    void LibrarianMainPage_Hour(MouseEvent event) {
+
+    }
+
+    @FXML
+    void LibrarianMainPage_InventoryManagment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LibrarianMainPage_LogOut(MouseEvent event) {
+
+    }
+
+    @FXML
+    void LibrarianMainPage_Messages(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LibrarianMainPage_ReturnBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LibrarianMainPage_Status(MouseEvent event) {
+
+    }
+
+    @FXML
+    void LibrarianMainPage_ViewMemberShipCard(ActionEvent event) {
+
+    }
+    
+	public void start(Stage arg0) throws Exception {
+		
+		this.Stage = new Stage();
+		this.loader = new FXMLLoader();
+		this.root = loader.load(getClass().getResource("/gui/Librarian_MainPage.fxml").openStream());
+		this.scene = new Scene(root);			
+		this.Stage.setScene(scene);
+		this.Stage.showAndWait();
+		
+	}
+
+}
