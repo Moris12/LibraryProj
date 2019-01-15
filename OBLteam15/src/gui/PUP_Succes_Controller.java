2c14110119eaf18014ad760eaa4245c326ddbbb6
shuhe <shuhe@10.10.7.16>
2019-01-15 18:08:42 +0200

    shahar-fix last update

--- OBLteam15/src/gui/PUP_Succes_Controller.java
+++ OBLteam15/src/gui/PUP_Succes_Controller.java
@@ -1,5 +1,35 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class PUP_Succes_Controller {
+	
+    @FXML
+    private Label PUPSucces_ActionNameSucces_LB;
+
+    @FXML
+    private Button PUPSucces_Cancel_BTN;
+    
+    private String ActionStr;
+
+    @FXML
+    void PUPSucces_ActionNameSucces() {
+
+    }
+
+    @FXML
+    void PUPSucces_Cancel() {
+
+    }
+
+	public String getActionStr() {
+		return ActionStr;
+	}
+
+	public void setActionStr(String actionStr) {
+		ActionStr = actionStr;
+	}
 
 }
